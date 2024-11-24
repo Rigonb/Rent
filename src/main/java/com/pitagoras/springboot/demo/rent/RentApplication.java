29646061028b1d69af4908f4c85b7cec4c027373
rigonbashota <bashotarigon6@gmail.com>
2024-11-25 00:17:26 +0100

    smthing more

--- src/main/java/com/pitagoras/springboot/demo/rent/RentApplication.java
+++ src/main/java/com/pitagoras/springboot/demo/rent/RentApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RentApplication {
 
     public static void main(String[] args) {
+
+
+
         SpringApplication.run(RentApplication.class, args);
 
     }
